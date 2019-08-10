469f7a4213a967976ff3410bef439700faa6e8ec
Luis Miguel <inf.lrodriguez@gmail.com>
2019-08-10 04:06:46 -0400

    Styled JSX - Estilos CSS

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,44 @@
-export default function index() {
+export default function() {
     return (
-        <>
+        <div>
             <h1>¡Hola Mundo en NextJS! </h1>
-            <p>Esperando aprender mucho en este curso.</p>
-        </>
+            <img src="./static/nextjs.png" alt="wifi"/>
+            <style jsx>{`
+            div{
+                height: 100vh;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                font-family: nunito, sans-serif;
+                
+            
+            }
+            img{
+                width: 200px;
+                height: 200pxpx;
+
+            }
+            h1{
+                text-align: center;
+            }
+            `}
+            </style>
+
+            <style jsx global>{`
+                *{
+                    margin: 0;
+                    padding: 0;
+                    box-sizing: border-box;
+                }
+                body{
+                    background-color: #f7be16;
+                }
+
+            `}</style>
+        </div>
+
+
+
     )
 }
