3c0bc2c28a42274dbf7efd35b6052cb21ed23a6a
Luis Miguel <inf.lrodriguez@gmail.com>
2019-08-10 03:26:28 -0400

    Primera pagina con NextJS

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function index() {
+    return (
+        <>
+            <h1>¡Hola Mundo en NextJS! </h1>
+            <p>Esperando aprender mucho en este curso.</p>
+        </>
+    )
+}
