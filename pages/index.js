4abb300d3acd20e84011851dc41425e295893090
Luis Miguel <inf.lrodriguez@gmail.com>
2019-08-10 23:09:50 -0400

    getInitialProps - Fetch a API de audioboom

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,69 @@
-export default function() {
+import fetch from 'isomorphic-unfetch';
+
+function Home({ channels }) {
     return (
         <div>
-            <h1>¡Hola Mundo en NextJS! </h1>
-            <img src="./static/nextjs.png" alt="wifi"/>
-            <style jsx>{`
-            div{
-                height: 100vh;
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-                align-items: center;
-                font-family: nunito, sans-serif;
-                
-            
-            }
-            img{
-                width: 200px;
-                height: 200pxpx;
-
-            }
-            h1{
+            <header> Podcast </header>
+            <div className="channels">
+                {channels.map(channel => (
+                    <div className="channel" key={channel.id}>
+                        <img src={channel.urls.logo_image.original} alt=""/>
+                        <h2>{channel.title}</h2>
+                    </div>
+                )) 
+                }
+            </div>
+        <style jsx>{`
+            header {
+                color: #fff;
+                background: #8756ca;
+                padding: 15px;
                 text-align: center;
+                font-size: 1.25rem;
+                font-weight: bold;
             }
-            `}
-            </style>
-
-            <style jsx global>{`
-                *{
-                    margin: 0;
-                    padding: 0;
-                    box-sizing: border-box;
+            .channels {
+                display: grid;
+                grid-gap: 15px;
+                padding: 15px;
+                grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+            }
+            .channel {
+                display: block;
+                margin-bottom: 0.5em;
+                color: #333;
+                text-decoration: none;
+                
+            }
+            .channel img {
+                border-radius: 3px;
+                box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
+                width: 100%;
+            }
+            h2 {
+                padding: 5px;
+                font-size: 0.9em;
+                font-weight: 600;
+                margin: 0;
+                text-align: center;
                 }
-                body{
-                    background-color: #f7be16;
-                }
+        `}</style>
 
-            `}</style>
-        </div>
-
-
-
+        <style jsx global>{`
+            body {
+                margin: 0;
+                font-family: system-ui;
+                background: white;
+            }
+        `}</style>
+    </div>
     )
 }
+
+Home.getInitialProps = async () => {
+    const req = await fetch('https://api.audioboom.com/channels/recommended')
+    const { body : channels} = await req.json()
+    return { channels }
+}
+
+export default Home
